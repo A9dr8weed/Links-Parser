95c96097d6b99fde738cc897304c504af1590e5c
Andrew <andsof123455@gmail.com>
2021-01-26 13:02:34 +0200

    Add some fix

--- Links Parser/Links_Parser.py
+++ Links Parser/Links_Parser.py
@@ -14,6 +14,7 @@ RESET = colorama.Fore.RESET
 internal_urls = set()
 external_urls = set()
 
+# number of urls visited so far will be stored here
 total_urls_visited = 0
 
 def is_valid(url):
@@ -21,7 +22,10 @@ def is_valid(url):
     Checks whether `url` is a valid URL.
     """
     parsed = urlparse(url)
+
+    # This will make sure that a proper scheme (protocol, e.g http or https) and domain name exists in the URL.
     return bool(parsed.netloc) and bool(parsed.scheme)
+
 
 def get_all_website_links(url):
     """
@@ -29,19 +33,27 @@ def get_all_website_links(url):
     """
     # all URLs of `url`
     urls = set()
+
     # domain name of the URL without the protocol
     domain_name = urlparse(url).netloc
+
+    # downloaded the HTML content of the web page and wrapped it with a soup object to ease HTML parsing.
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
     for a_tag in soup.findAll("a"):
         href = a_tag.attrs.get("href")
+
         if href == "" or href is None:
             # href empty tag
             continue
+
         # join the URL if it's relative (not absolute link)
         href = urljoin(url, href)
         parsed_href = urlparse(href)
+
         # remove URL GET parameters, URL fragments, etc.
         href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+
         if not is_valid(href):
             # not a valid URL
             continue
@@ -50,40 +62,49 @@ def get_all_website_links(url):
             continue
         if domain_name not in href:
             # external link
+            if href not in external_urls:
+                print(f"{GRAY}[!] External link: {href}{RESET}")
+                external_urls.add(href)
             continue
+
+        print(f"{GREEN}[*] Internal link: {href}{RESET}")
         urls.add(href)
+        internal_urls.add(href)
     return urls
 
-def crawl(url, max_urls=50):
+
+def crawl(url, max_urls = 30):
     """
     Crawls a web page and extracts all links.
     You'll find all links in `external_urls` and `internal_urls` global set variables.
     params:
-        max_urls (int): number of max urls to crawl, default is 30.
+        max_urls (int): number of max urls to crawl.
     """
     global total_urls_visited
     total_urls_visited += 1
     links = get_all_website_links(url)
+
     for link in links:
         if total_urls_visited > max_urls:
             break
-        crawl(link, max_urls=max_urls)
+        crawl(link, max_urls = max_urls)
+
 
 if __name__ == "__main__":
-    crawl("https://www.marvel.com/characters")
+    crawl("https://rozetka.com.ua/ua/notebooks/c80004/")
 
     print("[+] Total Internal links:", len(internal_urls))
     print("[+] Total External links:", len(external_urls))
     print("[+] Total URLs:", len(external_urls) + len(internal_urls))
 
-    domain_name = urlparse(url).netloc
+    domain_name = urlparse("https://rozetka.com.ua/ua/notebooks/c80004/").netloc
 
     # save the internal links to a file
-    with open(f"{domain_name}_internal_links.txt", "w") as f:
+    with open(f"C:\\Users\\Andrew\\Desktop\\{domain_name}_internal_links.txt", "w") as f:
         for internal_link in internal_urls:
             print(internal_link.strip(), file=f)
 
     # save the external links to a file
-    with open(f"{domain_name}_external_links.txt", "w") as f:
+    with open(f"C:\\Users\\Andrew\\Desktop\\{domain_name}_external_links.txt", "w") as f:
         for external_link in external_urls:
             print(external_link.strip(), file=f)
